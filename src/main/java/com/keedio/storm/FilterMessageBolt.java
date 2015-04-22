95810c5bc901276eb259c7281c41f212574deba5
Rodrigo Olmo Velasco <rolmo@MacBook-Pro-de-Rodrigo.local>
2015-04-22 15:59:00 +0200

    Functional review as expected

--- src/main/java/com/keedio/storm/FilterMessageBolt.java
+++ src/main/java/com/keedio/storm/FilterMessageBolt.java
@@ -1,10 +1,18 @@
 package com.keedio.storm;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,7 +41,10 @@ public class FilterMessageBolt implements IBasicBolt {
     private StormYammerMetricsAdapter yammerAdapter;
 	private Counter rejected;
 	private Counter accepted;
-    private Histogram throughput;    
+    private Histogram throughput;   
+    private String groupSeparator;
+    //private String pattern;
+    private Map<String, String> allPatterns;
     
 	public Counter getRejected() {
 		return rejected;
@@ -75,12 +86,35 @@ public class FilterMessageBolt implements IBasicBolt {
 	public void prepare(Map stormConf, TopologyContext context) {
 		allowMessages = (String) stormConf.get("filter.bolt.allow");
 		denyMessages = (String) stormConf.get("filter.bolt.deny");
+		//pattern = (String) stormConf.get("pattern");
+		groupSeparator = (String) stormConf.get("group.separator");
+		allPatterns = getPropKeys(stormConf, "conf");
         
         yammerAdapter = StormYammerMetricsAdapter.configure(stormConf, context, new MetricsRegistry());
         accepted = yammerAdapter.createCounter("accepted", "");
         rejected = yammerAdapter.createCounter("rejected", "");
         throughput = yammerAdapter.createHistogram("throughput", "", false);
 		
+	}
+
+	private Map<String, String> getPropKeys(Map stormConf, String pattern) {
+		Map<String,String> set = new HashMap<String,String>();
+		Pattern pat = Pattern.compile(pattern + "\\." + "(\\w*)");
+		
+		Iterator<String> it = stormConf.keySet().iterator();
+		
+		while (it.hasNext()) {
+			String key = it.next();
+			Matcher m = pat.matcher(key);
+			
+			if (m.find()) {
+				String value = (String)stormConf.get(key);
+				set.put(m.group(1), value);
+			}
+				
+		}
+
+		return set;
 	}
 
 	@Override
@@ -103,8 +137,15 @@ public class FilterMessageBolt implements IBasicBolt {
 			Matcher matcherAllow = patternAllow.matcher(message);
 			if (matcherAllow.find()) {
 				LOG.debug("Emiting tuple(allowed): " + message.toString());
-				collector.emit(tuple(message.getBytes()));
-				accepted.inc();
+				try {
+					String nextMessage = filterMessage(message);
+					System.out.println(nextMessage);
+					collector.emit(tuple(nextMessage.getBytes()));
+					accepted.inc();
+				} catch (ParseException e) {
+					LOG.error("error al realizar el filtrado de datos", e);
+					rejected.inc();
+				}
 			}else{
 				LOG.debug("NOT Emiting tuple(not allowed): " + message.toString());
 				rejected.inc();
@@ -116,8 +157,15 @@ public class FilterMessageBolt implements IBasicBolt {
 			Matcher matcherDeny = patternDeny.matcher(message);
 			if (!matcherDeny.find()) {
 				LOG.debug("Emiting tuple(not denied): " + message.toString());
-				collector.emit(tuple(message.getBytes()));
-				accepted.inc();
+				try {
+					String nextMessage = filterMessage(message);
+					System.out.println(nextMessage);
+					collector.emit(tuple(nextMessage.getBytes()));
+					accepted.inc();
+				} catch (ParseException e) {
+					LOG.error("error al realizar el filtrado de datos", e);
+					rejected.inc();
+				}
 			}else {
 				rejected.inc();
 				LOG.debug("NOT Emiting tuple(denied): " + message.toString());
@@ -125,12 +173,64 @@ public class FilterMessageBolt implements IBasicBolt {
 		}
 		else{
 			LOG.debug("Emiting tuple(no filter): " + message.toString());
-			collector.emit(tuple(message.getBytes()));
-			accepted.inc();
+			try {
+				String nextMessage = filterMessage(message);
+				System.out.println(nextMessage);
+				collector.emit(tuple(nextMessage.getBytes()));
+				accepted.inc();
+			} catch (ParseException e) {
+				LOG.error("error al realizar el filtrado de datos", e);
+				rejected.inc();
+			}
 		}
 
 	}
 
+	public String filterMessage(String message) throws ParseException {
+		
+		JSONParser parser = new JSONParser();  
+		
+		// El mensaje recibido es del tipo {"extraData":"...", "message":"..."}
+		JSONObject obj = (JSONObject)parser.parse(message);
+		// Aplicamos el filtro para obtener map con resultados
+		String originalMessage = (String) obj.get("message");
+		StringBuffer buf = new StringBuffer();
+		Iterator<String> it = allPatterns.keySet().iterator();
+		
+		while (it.hasNext()) {
+			String key = it.next();
+			String pattern = allPatterns.get(key);
+			
+			String all = getFilteredMessages(pattern, originalMessage);
+			
+			buf.append(all).append(groupSeparator);
+		}
+		
+		obj.put("message", buf.toString());
+		
+		return obj.toJSONString();
+	}
+
+	private String getFilteredMessages(String pattern, String message) throws ParseException {
+
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(message);
+		
+		StringBuffer buf = new StringBuffer();
+		if (m.find()) {
+			int count = m.groupCount();
+			
+			for (int i=1;i<=count;i++) {
+				buf.append(m.group(i));
+				if (i != count)
+					buf.append(groupSeparator);
+			}
+		}
+		
+
+		return buf.toString();
+	}
+	
 	@Override
 	public void cleanup() {
 		// TODO Auto-generated method stub
---
+++ src/test/java/com/keedio/storm/FilterBoltTest.java
@@ -0,0 +1,63 @@
+package com.keedio.storm;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+
+import org.junit.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+import backtype.storm.Config;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.tuple.Tuple;
+
+public class FilterBoltTest {
+
+	ServerSocket ss;
+	private FilterMessageBolt bolt;
+	
+	@Mock
+	private TopologyContext topologyContext = mock(TopologyContext.class);
+
+	@Mock
+	private BasicOutputCollector collector = mock(BasicOutputCollector.class);
+
+	@Before
+	public void setUp() throws UnknownHostException, IOException {
+		bolt = new FilterMessageBolt();
+		Config conf = new Config();
+		conf.put("filter.bolt.allow", ""); // Aceptamos las cadenas con tres digitos seguidos
+		conf.put("filter.bolt.deny", ""); // Rechazamos las cadenas que 
+		conf.put("metrics.reporter.yammer.facade..metric.bucket.seconds", 10);
+		conf.put("conf.pattern1", "(<date>[^\\s]+)\\s+(<time>[^\\s]+)\\s+");
+		conf.put("conf.pattern2", "(<date>[^\\s]+)\\s+");
+		conf.put("group.separator", "|");
+		bolt.prepare(conf, topologyContext);
+	}
+
+	@After
+	public void finish() throws IOException {
+		ss.close();
+	}
+	
+	@Test
+	public void testAccepted() throws InterruptedException {
+		Tuple tuple = mock(Tuple.class);
+		
+		String ret = "{\"extraData\":\"fsfsdf\",\"message\":\"hola amigo <date>11-23-24  <time>22:22:22 sflhsldfjs\"}";
+		
+	    when(tuple.getBinary(anyInt())).thenReturn(ret.getBytes());
+		bolt.execute(tuple, collector);
+		
+		Assert.assertTrue("Se acepta", bolt.getAccepted().count() == 1);
+
+	}
+	
+
+}
